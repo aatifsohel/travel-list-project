ee73f15d2c981247840cc2e1e280db5893b5438a
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2025-09-10 19:21:36 +0530

    add project files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+// ];
+
+function App() {
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  );
+}
+
+export default App;
