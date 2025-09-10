f12315a45f157a756110b5c0c3f2376bcb00a4a2
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2025-09-10 22:44:47 +0530

    setup layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,36 @@
 
 function App() {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
   );
 }
 
